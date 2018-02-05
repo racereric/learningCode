876fc10fa7db0934e876ef02fca800d380c1e760
racereric <racereric724@comcast.net>
2018-02-05 17:07:41 -0500

    Took out infeed errors

--- src/org/usfirst/frc/team5401/robot/subsystems/Infeed.java
+++ src/org/usfirst/frc/team5401/robot/subsystems/Infeed.java
@@ -38,7 +38,7 @@ public class Infeed extends Subsystem {
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
-    	setDefaultCommand(new FeederControl());
+    	//setDefaultCommand(new FeederControl());
     }
     
     public void feedDirection(int direction){
